79dd35be0f9e6f7503881f7d5c99b20788544868
david <ostermann.david@gmail.com>
2017-01-04 12:19:48 +0100

    change folder names

--- exercices/prompt-game/index.js
+++ exercices/prompt-game/index.js
@@ -1,30 +1,16 @@
 ( function() {
-
-  var responseContainer = document.querySelector('#response');
-  var idCard = document.querySelector('#side');
-
-  var btn = document.querySelector('#btn');
-
-  var arrPerso = [
-    {nom: 'obi-wan', side: 'lumineux'},
-    {nom: 'dark vador', side: 'obscur'},
-    {nom: 'palpatine', side: 'obscur'},
-    {nom: 'luke', side: 'lumineux'}
-  ]
-
-  var btnClickHandler = function(e) {
-
-    var userResponse = responseContainer.value;
-
-    var arrResult = arrPerso.filter( function(item) {
-        return item.nom === userResponse;
-    });
-
-    idCard.innerHTML = arrResult[0].side;
-  };
   
-  //btn.onclick = btnClickHandler;
-  btn.addEventListener('click', btnClickHandler);
-  console.log('btn: ', btn);
+  var resultContainer = document.getElementById('result');
+  console.log('test1');
+  var reponse1 = prompt('joueur 1. Ecris une lettre ?');
+  console.log('test2');
+  var reponse2 = prompt('joueur 2. Ecris une lettre ?');
+  console.log('test3');
+  debugger;
+  if(reponse1 === reponse2) {
+    resultContainer.innerHTML ='Right';
+  } else {
+    resultContainer.innerHTML ='Wrong';
+  }
 
 })();
